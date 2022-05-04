d6cacd1423f2f55ff7325c1e208dc0cc513a9dd4
Maxim Zelensky <maxim.zelensky@soxes.ch>
2022-05-04 14:21:06 +0200

    fix simon says algorithm + comments

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,3 +1,4 @@
+// import all sound effects
 let redAudio = new Audio('./sources/audio/red.mp3')
 let blueAudio = new Audio('./sources/audio/blue.mp3')
 let yellowAudio = new Audio('./sources/audio/yellow.mp3')
@@ -5,10 +6,7 @@ let greenAudio = new Audio('./sources/audio/green.mp3')
 let winAudio = new Audio('./sources/audio/you_won.mp3')
 let wrongAudio = new Audio('./sources/audio/wrong.mp3')
 
-let randList = []
-let difficulty = 4
-let score = 0
-
+// import all buttons
 let redBtn = document.getElementById("red")
 let blueBtn = document.getElementById("blue")
 let yellowBtn = document.getElementById("yellow")
@@ -16,141 +14,193 @@ let greenBtn = document.getElementById("green")
 let startBtn = document.getElementById("start-btn")
 let repeatBtn = document.getElementById("repeat-btn")
 
+// simons says diff array
+let randList = []
+// check diff pointer
+let pointer = 0
+
+// load last game session on page load
 function loadScore(){
-	score =	localStorage.getItem('gameScore')
-	difficulty = localStorage.getItem('gameDifficulty')
-	if(score === null || difficulty === null){
-		difficulty = 4
-		score = 0
-	} else{
-		score = parseInt(score)
-		difficulty = parseInt(difficulty)
+	// get saved diff array from local storage
+	randList = JSON.parse(localStorage.getItem('lastGame'))
+	// if no array was found, array must be empty
+	if(randList === null || randList.length === 0){
+		randList = []
 	}
-	document.getElementById("score").innerHTML = score
-	console.log("old score loaded")
+	// load old score
+	document.getElementById("score").innerHTML = randList.length * 50
+	console.log("Last session loaded.")
 }
 
+// start new game
 function startNewGame(){
-	randList = []
-	for (step = 0; step < difficulty; step++) {
-		randList.push(Math.floor(Math.random() * 4) + 1)
-	}
+	// add random number to array
+	randList.push(Math.floor(Math.random() * 4) + 1)
 	console.log(randList)
 	console.log("New Game")
+	// show saimon says diff
 	showDiff()
 }
 
+// show saimon says diff on game field
 function showDiff(){
-	if(randList.length === difficulty){
-		blockBtns(true)
-		i = 0
-		randList.forEach((e) => {
-			setTimeout(() => {
-				if(e === 1){
-					redBtn.style.backgroundColor = "darkred"
-					redAudio.play().then(()=>{
-						setTimeout(() => {
-							redBtn.style.backgroundColor = "red"
-						}, 500);
-					})
-				} else if(e === 2){
-					blueBtn.style.backgroundColor = "darkblue"
-					blueAudio.play().then(()=>{
-						setTimeout(() => {
-							blueBtn.style.backgroundColor = "blue"
-						}, 500);
-					})
-				} else if(e === 3){
-					yellowBtn.style.backgroundColor = "darkgoldenrod"
-					yellowAudio.play().then(()=>{
-						setTimeout(() => {
-							yellowBtn.style.backgroundColor = "yellow"
-						}, 500);
-					})
-				} else if(e === 4){
-					greenBtn.style.backgroundColor = "darkgreen"
-					greenAudio.play().then(()=>{
-						setTimeout(() => {
-							greenBtn.style.backgroundColor = "green"
-						}, 500);
-					})
-				}
-			}, i * 1000)
-			i += 1
-		})
+	// block all buttons
+	blockBtns(true)
+	// timeout timer pointer
+	i = 0 
+	// show all buttons
+	randList.forEach((e) => {
 		setTimeout(() => {
-			blockBtns(false)
-		}, i* 1000)
-	}
+			// show red button
+			if(e === 1){
+				// make red button background dark
+				redBtn.style.backgroundColor = "darkred"
+				// play audio sample
+				redAudio.play().then(()=>{
+					// wait 0.5 second before changing color back
+					setTimeout(() => {
+						// change background color back
+						redBtn.style.backgroundColor = "red"
+					}, 500);
+				})
+			} 
+			// show blue button
+			else if(e === 2){
+				// make blue button background dark
+				blueBtn.style.backgroundColor = "darkblue"
+				// play audio sample
+				blueAudio.play().then(()=>{
+					// wait 0.5 second before changing color back
+					setTimeout(() => {
+						// change background color back
+						blueBtn.style.backgroundColor = "blue"
+					}, 500);
+				})
+			} 
+			// show yellow button
+			else if(e === 3){
+				// make yellow button background dark
+				yellowBtn.style.backgroundColor = "darkgoldenrod"
+				// play audio sample
+				yellowAudio.play().then(()=>{
+					// wait 0.5 second before changing color back
+					setTimeout(() => {
+						// change background color back
+						yellowBtn.style.backgroundColor = "yellow"
+					}, 500);
+				})
+			} 
+			// show green button
+			else if(e === 4){
+				// make green button background dark
+				greenBtn.style.backgroundColor = "darkgreen"
+				// play audio sample
+				greenAudio.play().then(()=>{
+					// wait 0.5 second before changing color back
+					setTimeout(() => {
+						// change background color back
+						greenBtn.style.backgroundColor = "green"
+					}, 500);
+				})
+			}
+		}, i * 1000)
+		i += 1
+	})
+	// wait all audios before unblock buttons back
+	setTimeout(() => {
+		// unblock buttons
+		blockBtns(false)
+	}, i* 1000)
 }
 
+// red button press handler
 function pressRedBtn(){
+	// play audio sample
 	redAudio.play().then(() => {
 		console.log("Red")
+		// check diff
 		checkEntry(1)
 	})
 }
 
+// blue button press handler
 function pressBlueBtn(){
+	// play audio sample
 	blueAudio.play().then(() => {
 		console.log("Blue")
+		// check diff
 		checkEntry(2)
 	})
 }
 
+// yellow button press handler
 function pressYellowBtn(){
+	// play audio sample
 	yellowAudio.play().then(() => {
 		console.log("Yellow")
+		// check diff
 		checkEntry(3)
 	})
 }
 
+// green button press handler
 function pressGreenBtn(){
+	// play audio sample
 	greenAudio.play().then(() => {
 		console.log("Green")
+		// check diff
 		checkEntry(4)
 	})
 }
 
+// check user entry with simon says diff
 function checkEntry(btn){
+	// diff array cant be empty
 	if(randList.length != 0){
-		if(btn === randList[0]){
-			randList.shift()
-			if(randList.length === 0){
+		// check if button correct
+		if(btn === randList[pointer]){
+			// update pointer for next user entry
+			pointer += 1
+			// if entry is last
+			if(randList.length === pointer){
+				// user wins
 				win()
 			}
 		} else{
+			// user looses
 			wrong()
 		}
 	}
 }
 
+// user win
 function win(){
 	winAudio.play().then(() => {
 		console.log("win")
-		score += 50
-		difficulty += 1
-		localStorage.setItem('gameScore', score)
-		localStorage.setItem('gameDifficulty', difficulty)
-		document.getElementById("score").innerHTML = score
+		pointer = 0
+		localStorage.setItem('lastGame', JSON.stringify(randList))
+		document.getElementById("score").innerHTML = randList.length * 50
 		setTimeout(() => {
 			startNewGame()
 		}, 2000);
 	})
 }
 
+// user loose
 function wrong(){
+	// reset array and pointer
 	randList = []
+	pointer = 0
+	// play looser audio
 	wrongAudio.play().then(() => {
 		console.log("loose")
-		score = 0
-		difficulty = 4
-		localStorage.removeItem('gameScore')
-		localStorage.removeItem('gameDifficulty')
+		// clear score and local storage
+		localStorage.removeItem('lastGame')
 		document.getElementById("score").innerHTML = 0
 	})
 }
 
+// block buttons
 function blockBtns(status){
 	redBtn.disabled = status
 	blueBtn.disabled = status
